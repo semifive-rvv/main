ba994024fe79395203e30c52c00b0b5f7ab4f137
Jasper Song <jasper.song@semifive.com>
2021-05-28 12:30:58 +0900

    - .gitignore 추가 - 결과물들 (bd, logs, results) git repo에서 삭제 - compiler , spike, pk 등의 path등을 변경

--- embench-iot/compile_embench.bash
+++ embench-iot/compile_embench.bash
@@ -2,7 +2,7 @@
 
 # Native compilation: ./build_all.py --arch native --board default --chip default --clean
 
-EMBENCH_ROOT=/DATADRIVE1/Projects/RISCV/BENCHMARKS/embench-iot
+EMBENCH_ROOT=$PWD
 
 if [ "$#" -ne 1 ]; then
   echo "Compiling Embench "
@@ -11,6 +11,7 @@ if [ "$#" -ne 1 ]; then
   echo "     mode = 2: RISCV: Just compiling embench"
   echo "     mode = 3: NATIVE: Clearing previous results and compiling embebch"
   echo "     mode = 4: NATIVE: Just compiling embench"
+  echo "     mode = 5: RISCV : SEMIFICVE env."
   exit 2
 fi
 
@@ -20,14 +21,14 @@ case $1 in
     echo "### RISCV: Clearing previous results and compiling embench..."
     echo "----------------------------------------------"
     echo ""
-	$EMBENCH_ROOT/build_all.py --arch riscv32 --chip generic --board spike --cc /home/jong/Projects/RISCV/bin/riscv32-unknown-elf-gcc --clean --verbose
+	$EMBENCH_ROOT/build_all.py --arch riscv32 --chip generic --board spike --cc $RISCV_PATH/bin/riscv32-unknown-elf-gcc --clean --verbose
     ;;
   "2")
     echo "### RISCV: Compiling embench ..."
     echo "----------------------------------------------"
     echo ""
 	echo "### RISCV: Run build_all.py ..."
-	$EMBENCH_ROOT/build_all.py --arch riscv32 --chip generic --board spike --cc /home/jong/Projects/RISCV/bin/riscv32-unknown-elf-gcc --verbose
+	$EMBENCH_ROOT/build_all.py --arch riscv32 --chip generic --board spike --cc $RISCV_PATH/bin/riscv32-unknown-elf-gcc --verbose
     ;;
   "3")
     echo "### NATIVE: Clearing previous results and compiling embench..."
@@ -42,10 +43,16 @@ case $1 in
 	echo "### NATIVE: Run build_all.py ..."
 	$EMBENCH_ROOT/build_all.py --arch native --board default --chip default --verbose
     ;;
+  "5")
+    echo "### RISCV: Clearing previous results and compiling embench..."
+    echo "----------------------------------------------"
+    echo ""
+	$EMBENCH_ROOT/build_all.py --arch riscv32 --chip generic --board spike --cc $RISCV_PATH/bin/riscv32-unknown-elf-gcc  --verbose --clean \
+        --cflags "-march=rv32imc -mabi=ilp32" 
+    ;;
 
   *)
-    echo "Wrong mode. MODE must be one of 1, 2, 3 or 4"
+    echo "Wrong mode. MODE must be one of 1, 2, 3, 4 or 5"
     exit 2;;
 esac
-
 
